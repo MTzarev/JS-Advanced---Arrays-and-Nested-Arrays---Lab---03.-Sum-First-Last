fb281e61d9cb8698b7abb8a0fd48b76000d19748
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-10 22:51:09 +0300

    Add files via upload

---
+++ 3.SumFirstLast.js
@@ -0,0 +1,10 @@
+function sumFirstLast(array) {
+
+let start = array[0]
+
+let end = array[array.length-1]
+let result = Number(start)+Number(end)
+
+return result
+}
+console.log(sumFirstLast(['20', '30', '40']))
