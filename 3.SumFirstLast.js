a6cb8afd94609f464a35464f314cbef1e41d88e5
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-24 22:18:54 +0300

    Update 3.SumFirstLast.js

--- 3.SumFirstLast.js
+++ 3.SumFirstLast.js
@@ -1,10 +1,8 @@
 function sumFirstLast(array) {
 
 let start = array[0]
-
 let end = array[array.length-1]
 let result = Number(start)+Number(end)
-
 return result
 }
 console.log(sumFirstLast(['20', '30', '40']))
